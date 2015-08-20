5e4b1571de61a0b18f756ebe1f0ff8ae836b1a12
yuma <yuma1192yuma2960@yahoo.co.jp>
2015-08-21 04:13:53 +0900

    カード使えるようになった

--- Project3/Project3/TranpGame.cpp
+++ Project3/Project3/TranpGame.cpp
@@ -26,6 +26,7 @@ static map<int, string> TOTRANP = {
 	{ 11,"‚i" },
 	{ 12,"‚p" },
 	{ 13,"‚j" },
+	{14,"Jo"}
 };
 
 TranpGame::TranpGame(const int aopponent,vector<string> atexts, gameSceneChanger* changer) 
@@ -43,25 +44,64 @@ void TranpGame::Initialize() {
 }
 
 void TranpGame::Update() {
-	
+	int mychoosenum = 0;
+	for (int i = 0; i < 9; ++i) {
+		if (Keyboard_Get('1' + i)) {
+			mycarduseds[i] = true;
+			mychoosenum = i+1;
+			break;
+		}
+	}
+	if (mychoosenum == 0) {
+		if (Keyboard_Get('0')) {
+			mycarduseds[9] = true;
+			mychoosenum = 10;
+		}
+		else if (Keyboard_Get('J')) {
+			mycarduseds[10] = true;
+			mychoosenum = 11;
+		}
+		else if (Keyboard_Get('Q')) {
+			mycarduseds[11] = true;
+			mychoosenum = 12;
+		}
+		else if (Keyboard_Get('K')) {
+			mycarduseds[12] = true;
+			mychoosenum = 13;
+		}
+	}
 }
 void TranpGame::Draw() {
 	assert(false);
 }
 void TranpGame::Draw(vector<string> &tmpfield) {
-	tmpfield[0].replace(0, 2, "‚à‚¤Ž€‚ñ‚¾‚Í‚¸‚¶‚á‚È‚©‚Á‚½‚Ì‚©");
-	tmpfield[SIZE_Y - 1].replace(0, 2, "‚à‚¤Ž€‚ñ‚¾‚Í‚¸‚¶‚á‚È‚©‚Á‚½‚Ì‚©");
+	switch (opponent) {
+	case 0:
+		tmpfield[0].replace(0, 2, "‚à‚¤Ž€‚ñ‚¾‚Í‚¸‚¶‚á‚È‚©‚Á‚½‚Ì‚©");
+		tmpfield[SIZE_Y - 1].replace(0, 2, "‚à‚¤Ž€‚ñ‚¾‚Í‚¸‚¶‚á‚È‚©‚Á‚½‚Ì‚©");
+		tmpfield[OP_UNUSEDCARDLIST_UP].replace(OP_UNUSEDCARDLIST_LEFT-2, 2, "“~");
+		break;
+	case 1:
+		break;
+	case 2:
+		break;
+	}
+	
 	for (int i = 0; i < 7; ++i) {
-		tmpfield[OP_UNUSEDCARDLIST_UP].replace(OP_UNUSEDCARDLIST_LEFT+4*i, 2, TOTRANP[i+1]);
+		if(!opcarduseds[i])
+			tmpfield[OP_UNUSEDCARDLIST_UP].replace(OP_UNUSEDCARDLIST_LEFT+4*i, 2, TOTRANP[i+1]);
 	}
 	for (int i = 7; i < 13; ++i) {
-		tmpfield[OP_UNUSEDCARDLIST_UP+1].replace(OP_UNUSEDCARDLIST_LEFT+2+4*(i-7), 2, TOTRANP[i+1]);
+		if (!opcarduseds[i])
+tmpfield[OP_UNUSEDCARDLIST_UP+1].replace(OP_UNUSEDCARDLIST_LEFT+2+4*(i-7), 2, TOTRANP[i+1]);
 	}
 	for (int i = 0; i < 7; ++i) {
-		tmpfield[MY_UNUSEDCARDLIST_UP].replace(MY_UNUSEDCARDLIST_LEFT+4*i, 2, TOTRANP[i+1]);
+		if (!mycarduseds[i])
+tmpfield[MY_UNUSEDCARDLIST_UP].replace(MY_UNUSEDCARDLIST_LEFT+4*i, 2, TOTRANP[i+1]);
 	}
 	for (int i = 7; i < 13; ++i) {
-		tmpfield[MY_UNUSEDCARDLIST_UP + 1].replace(MY_UNUSEDCARDLIST_LEFT + 2 + 4 * (i-7), 2, TOTRANP[i + 1]);
+		if (!mycarduseds[i])
+			tmpfield[MY_UNUSEDCARDLIST_UP + 1].replace(MY_UNUSEDCARDLIST_LEFT + 2 + 4 * (i-7), 2, TOTRANP[i + 1]);
 	}
 	string oppost = to_string(oppoint);
 	if (oppost.size() % 2) {
