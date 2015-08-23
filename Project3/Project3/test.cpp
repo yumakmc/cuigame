82cc1e4d37e1440c8a15bcfb52aad5fb75747e7e
yuma <yuma1192yuma2960@yahoo.co.jp>
2015-08-23 23:11:38 +0900

    ダブルバッファできた

--- Project3/Project3/test.cpp
+++ Project3/Project3/test.cpp
@@ -16,19 +16,36 @@ int main(){
 	//while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0){
 	clock_t time = 0;
 	aTimeManager.GetDifference();
+	int i=0;
 	while (1){
+		i++;
+		i %= 2;
+		HANDLE hSrceen = CreateConsoleScreenBuffer(
+			GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL
+			);
+		if (hSrceen == INVALID_HANDLE_VALUE) {
+			scenemgr.Finalize();
+			return -1;
+		}
+		LPDWORD cell(0);
 		time += aTimeManager.GetDifference();
+
 		if (time > TPF) {
 			time -= TPF;
 			Keyboard_Update();
 			scenemgr.Update();
-			system("cls");
+			//system("cls");
 			vector<string> TmpField(SIZE_Y,"　　　　　　　　　　　　　　　　　　　　");//空白の数はSIZE_Xの数と同じ　修正したいけど重くなりそう
 			scenemgr.Draw(TmpField);
 			for (int y = 0; y < TmpField.size(); ++y) {
 				assert(TmpField[y].size() == 2 * SIZE_X);
-				cout << TmpField[y]<<endl;
+				const char* str = (TmpField[y]).c_str();
+				WriteConsole(hSrceen, str, strlen(str), cell, NULL);
+				const char* kaigyou = "\n";
+				WriteConsole(hSrceen, kaigyou, strlen(kaigyou), cell, NULL);
+				//cout << TmpField[y]<<endl;
 			}
+			SetConsoleActiveScreenBuffer(hSrceen);
 		}
 	}
 	scenemgr.Finalize();
