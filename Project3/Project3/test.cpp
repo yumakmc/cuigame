bf0a37e1f39bd37faabbdd800350977ca00acbf9
yuma <yuma1192yuma2960@yahoo.co.jp>
2015-08-24 15:16:54 +0900

    前の奴はめっちゃメモリくうので修正　なんか５行ほど下がるので修正したい

--- Project3/Project3/test.cpp
+++ Project3/Project3/test.cpp
@@ -14,40 +14,58 @@ int main(){
 	scenemgr.Initialize();
 	//aTimeManager
 	//while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0){
-	clock_t time = 0;
-	aTimeManager.GetDifference();
+	clock_t time = 0;	
 	int i=0;
-	while (1){
-		i++;
-		i %= 2;
-		HANDLE hSrceen = CreateConsoleScreenBuffer(
+	HANDLE hSrceen[2] = { CreateConsoleScreenBuffer(
+		GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL
+		), CreateConsoleScreenBuffer(
 			GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL
-			);
-		if (hSrceen == INVALID_HANDLE_VALUE) {
-			scenemgr.Finalize();
-			return -1;
-		}
-		LPDWORD cell(0);
-		time += aTimeManager.GetDifference();
+			) };
+	CONSOLE_CURSOR_INFO info = {};
+	
+	if (hSrceen[0] == INVALID_HANDLE_VALUE) {
+		scenemgr.Finalize();
+		return -1;
+	}
 
+	::GetConsoleCursorInfo(hSrceen[0], &info);
+	info.bVisible = false;
+	SetConsoleCursorInfo(hSrceen[0], &info);
+	::GetConsoleCursorInfo(hSrceen[1], &info);
+	info.bVisible = false;
+	SetConsoleCursorInfo(hSrceen[1], &info);
+
+	aTimeManager.GetDifference();
+	while (1){
+		time += aTimeManager.GetDifference();
 		if (time > TPF) {
 			time -= TPF;
 			Keyboard_Update();
 			scenemgr.Update();
-			//system("cls");
 			vector<string> TmpField(SIZE_Y,"　　　　　　　　　　　　　　　　　　　　");//空白の数はSIZE_Xの数と同じ　修正したいけど重くなりそう
 			scenemgr.Draw(TmpField);
+			LPDWORD cell(0);
 			for (int y = 0; y < TmpField.size(); ++y) {
 				assert(TmpField[y].size() == 2 * SIZE_X);
 				const char* str = (TmpField[y]).c_str();
-				WriteConsole(hSrceen, str, strlen(str), cell, NULL);
+				WriteConsole(hSrceen[i], str, strlen(str), cell, NULL);
 				const char* kaigyou = "\n";
-				WriteConsole(hSrceen, kaigyou, strlen(kaigyou), cell, NULL);
-				//cout << TmpField[y]<<endl;
+				WriteConsole(hSrceen[i], kaigyou, strlen(kaigyou), cell, NULL);
 			}
-			SetConsoleActiveScreenBuffer(hSrceen);
+			SetConsoleActiveScreenBuffer(hSrceen[i]);
+			DWORD wbyte;
+			COORD coord = {};
+			TCHAR str[801] = ("");
+			i++;
+			i %= 2;
+			::SetConsoleCursorPosition(hSrceen[i], coord);
+			::WriteConsole(hSrceen[i], str, static_cast<DWORD>(800), &wbyte, NULL);
 		}
 	}
+	CloseHandle(hSrceen[0]);
+	CloseHandle(hSrceen[1]);
+	hSrceen[0] = NULL;
+	hSrceen[1] = NULL;
 	scenemgr.Finalize();
 	return 0;
 }
