5962c7e2e90a195fd89f62c1b580eb95a02da6bc
yuma <yuma1192yuma2960@yahoo.co.jp>
2015-08-26 08:18:21 +0900

    戦ってる感だせるようになった

--- Project3/Project3/RpgGame.cpp
+++ Project3/Project3/RpgGame.cpp
@@ -1,14 +1,17 @@
 #include "RpgGame.h"
 #include "Keyboard.h"
 #include "DrawableConsole.h"
+#include "DrawBackGround.h"
 #include <string>
 
 const int ATTACK_CENTER_X = 19;
 const int ATTACK_CENTER_Y = 19;
 
+const int PARALYZECOUNT = 10;
+
 static const int dx[4] = { -1,0,1,0 };
 static const int dy[4] = { 0,-1,0,1 };
-RpgGame::RpgGame(gameSceneChanger* changer,const int aopponent) :gameBaseScene(changer), opponent(aopponent),op_hp(aopponent==0?50:(aopponent==1?70:100)){
+RpgGame::RpgGame(gameSceneChanger* changer,const int aopponent) :gameBaseScene(changer), opponent(aopponent),op_hp(GETENEMYINFO[opponent].hp), op_atk(GETENEMYINFO[opponent].atk), op_interval(GETENEMYINFO[opponent].interval){
 	Initialize();
 	;
 }
@@ -31,25 +34,32 @@ void RpgGame::Update() {
 				if (!rest_waittime[i]) {
 
 					//ダメ与える処理
-
+					op_hp -= my_atk[i];
 					rest_waittime[i] = max_waittime[i];
 				}
 				else {
-					//paralyzecount = 10;
+					paralyzecount = PARALYZECOUNT;
 					//しびれる処理
 				}
 			}
 		}
-		if (my_hp <= 0) {
-
-		}
-		else if (op_hp <= 0) {
-			//終了処理
-		}
 	}
-	
+	if (!(count % op_interval)) {
+		my_hp -= op_atk;
+	}
+	if (my_hp <= 0) {
+
+	}
+	else if (op_hp <= 0) {
+		//終了処理
+	}
 }
 void RpgGame::Draw() {
+	DrawBackGround(1);
+	aDrawableConsole.draw(17, 2, "敵：");
+	aDrawableConsole.draw(20, 2, GETENEMYINFO[opponent].name.c_str());
+	aDrawableConsole.draw(ATTACK_CENTER_X-5, ATTACK_CENTER_Y + 5, To_ZenString(op_hp).c_str());
+	aDrawableConsole.draw(ATTACK_CENTER_X+4, ATTACK_CENTER_Y + 5, To_ZenString(my_hp).c_str());
 	aDrawableConsole.draw(ATTACK_CENTER_X+dx[0], ATTACK_CENTER_Y+dy[0], "←");
 	aDrawableConsole.draw(ATTACK_CENTER_X + dx[1], ATTACK_CENTER_Y + dy[1], "↑");
 	aDrawableConsole.draw(ATTACK_CENTER_X + dx[2], ATTACK_CENTER_Y + dy[2], "→");
@@ -64,4 +74,12 @@ void RpgGame::Draw() {
 		
 	}
 	
+}
+
+string RpgGame::To_ZenString(const int anum) {
+	string st = to_string(anum);
+	if (st.size() % 2) {
+		st = " " + st;
+	}
+	return st;
 }
--- Project3/Project3/RpgGame.h
+++ Project3/Project3/RpgGame.h
@@ -1,5 +1,20 @@
 #pragma once
 #include "gameBaseScene.h"
+#include <map>
+
+struct EnemyInfo {
+	string name;
+	int hp;
+	int atk;
+	int interval;//攻撃の間隔
+	
+};
+
+static map<int, EnemyInfo> GETENEMYINFO = {
+	{0,{"？？？",365,5,60}},
+	{ 1,{"空",500,7,50 }},
+	{ 2,{"無限の空",888,12,70} },
+};
 
 class RpgGame :public gameBaseScene {
 public:
@@ -9,12 +24,16 @@ public:
 	void Update() override;        //更新処理をオーバーライド。
 	void Draw() override;            //描画処理をオーバーライド。
 private:
+	string inline To_ZenString(const int anum);
+
 	const int opponent;
-	const int op_hp;
+	int op_hp;
+	const int op_atk;
+	const int op_interval;
 
-	int my_hp=50;
+	int my_hp=365;
 
-	const int atk[4] = { 1,2,3,5 };
+	const int my_atk[4] = { 1,2,3,5 };
 	const int max_waittime[4] = { 5,8,11,17 };
 
 	int rest_waittime[4] = { 5,8,11,17 };
